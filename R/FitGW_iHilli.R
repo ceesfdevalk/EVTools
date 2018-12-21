f08a523145fe9f17a3db3922e13d52e52e942340
CeesdeValk <ceesfdevalk@gmail.com>
2018-12-21 08:33:58 +0100

    minor

--- R/FitGW_iHilli.R
+++ R/FitGW_iHilli.R
@@ -210,7 +210,7 @@ FitGW_iHilli <- function(X, p, N, r11, fixedpar, l0, sigma, XId) {
         }
         theta <- thetaref   # the refined estimator is the output
       } else {
-        ti <- theta[1]
+        ti <- theta0[1]
         temp <- cumsum(h(ti, th[1:(mk-1)]))/L[1:(mk-1)]
         w <- th[2:mk]^(-ti)*temp + h(ti, 1/th[2:mk])
         g <- hill0[l-1]/w[l-1]
