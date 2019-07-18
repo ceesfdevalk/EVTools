d2aeb9603511a251c7b4f3385082008a679fc65d
CeesdeValk <ceesfdevalk@gmail.com>
2019-07-18 13:42:28 +0200

    weet niet

--- R/selectThresholdP1.R
+++ R/selectThresholdP1.R
@@ -49,7 +49,7 @@ selectThresholdP1 <- function(theta, thetaStd, k, rthresh) {
   
   # the highly variable estimates of the index are excluded (not of interest, and noise spoils
   # the statistics of the fluctuations)
-  ind <- k> 20  
+  ind <- k> 20  # differenced estimates above this will be taken into accountxs
   k <- k[ind]
   theta <- theta[ind]
   thetaStd <- thetaStd[ind]
