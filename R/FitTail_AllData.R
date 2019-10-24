7b58e8aac3c51a5cfcdf01e405315fe63ea96150
CeesdeValk <ceesfdevalk@gmail.com>
2019-10-24 10:44:15 +0200

    log of quantile in threshold

--- R/FitTail_AllData.R
+++ R/FitTail_AllData.R
@@ -171,8 +171,8 @@ FitTail_AllData <- function(X, freq, df, method, options, metadata) {
   # Threshold choice
   if (length(pthreshold) <1) {
     if (!indexselect) {
-      Pthresh <- selectThresholdP1(estimates$quantile[, 1], estimates$quantileStd[, 1], estimates$l, 
-                                   0.9, kmin= kmin) 
+      Pthresh <- selectThresholdP1(log(estimates$quantile[, 1]), estimates$quantileStd[, 1]/estimates$quantile[, 1], 
+                                   estimates$l, 0.9, kmin= kmin) 
       li <- Pthresh$k[Pthresh$i]
       iselect <- which(estimates$l== li)
     } else {
