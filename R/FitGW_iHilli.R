3ff0cf8007c11088978869dbdb4ad95245d2a618
CeesdeValk <ceesfdevalk@gmail.com>
2019-10-24 17:33:34 +0200

    more pure

--- R/FitGW_iHilli.R
+++ R/FitGW_iHilli.R
@@ -308,7 +308,7 @@ FitGW_iHilli <- function(X, p, N, r11, fixedpar, l0, sigma, metadata) {
           deriv <- ha1 - g*ha*dw/hill0[l-1]
           var <- g^2*(ha^2*logdispStd^2+deriv^2*thetaStd^2) + X0lStd^2
           qStd[, i]= sqrt(var)
-          qStd[, i] <- rev(cummax(rev(qStd[, i])))  # to avoid unrealistic small values          
+          # qStd[, i] <- rev(cummax(rev(qStd[, i])))  # to avoid unrealistic small values          
         }
       }
     }
