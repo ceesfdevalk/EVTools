ad8ba01e4aca616b69ecbd51d551901a207f2f8d
CeesdeValk <ceesfdevalk@gmail.com>
2020-02-16 15:55:47 +0100

    assigncat sorted lbin, which was wrong!! now output= input

--- R/assigncat.R
+++ R/assigncat.R
@@ -1,52 +1,30 @@
 #
 # 
 #
-assigncat <- function(y, lbin, ubin, cats, binwidth) {
+assigncat <- function(y, lbin, ubin) {
   ly <- length(y)
   nbin <- length(lbin)
-  
-  if (length(binwidth)> 0 & (nbin== 0 & length(ubin)== 0)) {
-    nbin <- ceiling((max(y)-min(y))/binwidth)
-    cat <- (((y+binwidth/2) %/% binwidth) %% nbin)*binwidth
-    cats <- sort(unique(cat))
-    lbin <- cats-binwidth/2
-    ubin <- cats+binwidth/2
-    period <- ubin[nbin]-lbin[1]
-    lbin <- lbin %% period    
-    ubin <- ubin %% period  
-    ind <- sort(lbin, index.return= TRUE)$ix
-    lbin <- lbin[ind]
-    ubin <- ubin[ind]
-    cats <- cats[ind]
-    cat0 <- cat
-    cat <- rep(FALSE, ly*lbin)
-    dim(cat) <- c(ly, lbin)
-    cat[cbind(1:ly, cat0)] <- TRUE
-  } else if (nbin> 0 & length(ubin)== nbin) {
-    if (length(cats)!= nbin) {
-       cats <- lbin
-       warning("Bins (re)labelled by their lower bounds.")
-    }
-    if (nbin> 1) {
-      ibin <- sort(lbin, index.return= TRUE)$ix
-      lbin <- lbin[ibin]
-      ubin <- ubin[ibin]
-      cats <- cats[ibin]
+  cats <- NA
+  cat <- data.matrix(rep(TRUE, ly))
+  if (nbin> 1) {
+    if (length(ubin)!= nbin) {
+      stop("lbin and ubin have different lengths in $options$covariate.")
     } else {
-      cats <- NA
+      cats <- rep(NA, nbin)
+      cat <- rep(FALSE, ly*nbin)
+      dim(cat) <- c(ly, nbin)
+      for (i in 1:nbin) {
+        if (ubin[i]> lbin[i]) {
+          cat[, i] <- y>= lbin[i] & y< ubin[i]
+          cats[i] <- (ubin[i]+lbin[i])/2
+        } else {
+          cat[, i] <- y>= lbin[i] | y< ubin[i]  
+          cats[i] <- (((ubin[i]+lbin[i]-360)/2-.01) %% 360) + .01
+        }
+      }
     }
-    cat <- rep(FALSE, ly*nbin)
-    dim(cat) <- c(ly, nbin)
-    for (i in 1:nbin) {
-       if (ubin[i]> lbin[i]) {
-         cat[, i] <- y>= lbin[i] & y< ubin[i]
-       } else {
-         cat[, i] <- y>= lbin[i] | y< ubin[i]         
-       }
-    }
-  } else {  
-    stop("Inconsistencies/omissions in options$covariate found.")
   }
   value <- list(cat= cat, cats= cats, lbin= lbin, ubin= ubin)
 }
 
+  
